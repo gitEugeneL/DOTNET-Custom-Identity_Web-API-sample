ab9bb605e57a1ed6f1c34430bce83c688ba52e7f
Eugene L. <euglihon.dev@gmail.com>
2026-04-24 19:31:39 +0200

    added registration tests

--- Tests/Api.IntegrationTests/Features/RegistrationTests.cs
+++ Tests/Api.IntegrationTests/Features/RegistrationTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using Api.Constants;
+using Api.Domain.Entities;
 using Api.Features.Registration;
 
 namespace Api.IntegrationTests.Features;
@@ -12,7 +13,7 @@ public class RegistrationTests(ApiWebApplicationFactory factory) : IClassFixture
     [Theory]
     [InlineData("mailt@mail.test", "strongPwd!1", "strongPwd!1")]
     [InlineData("mail1@mail.test", "myPassword12@", "myPassword12@")]
-    public async Task Registration_WithValidBody_ReturnsUserUd
+    public async Task Registration_WithValidData_ReturnsCreatedResultWithUserId
         (string email, string password, string confirmPassword)
     {
         // Arrange
@@ -27,5 +28,27 @@ public class RegistrationTests(ApiWebApplicationFactory factory) : IClassFixture
 
         var result = await response.Content.ReadFromJsonAsync<Guid>();
         Assert.NotEqual(Guid.Empty, result);
+    }
+
+    [Theory]
+    [InlineData("mailt@mail.test", "strongPwd!1", "strongPwd!1")]
+    [InlineData("mail1@mail.test", "myPassword12@", "myPassword12@")]
+    public async Task Registration_WithConflictEmail_ReturnsConflictResultWithConflictMessage
+        (string email, string password, string confirmPassword)
+    {
+        // Arrange
+        var request = new RegistrationRequest(email, password, confirmPassword);
+
+        // Act
+        var response = new HttpResponseMessage();
+        for (var i = 0; i < 2; i++)
+            response = await _client.PostAsJsonAsync(ApiPaths.Registration, request);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+
+        var result = await response.Content.ReadFromJsonAsync<string>();
+        Assert.Equal(ApiMessages.ConflictResultMessage(nameof(User), email), result);
     }
 }
